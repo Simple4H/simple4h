1f292afca0fb533d7049554bf00ab717b8bfe348
simple4h <crescentcxm@gmail.com>
2019-12-03 11:16:55 +0800

    添加私服maven

--- simple4h-product/src/test/java/com/simple4h/Simple4hProductApplicationTests.java
+++ simple4h-product/src/test/java/com/simple4h/Simple4hProductApplicationTests.java
@@ -1,13 +1,18 @@
 package com.simple4h;
 
+import com.simple4h.common.DateUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class Simple4hProductApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Date date = new Date();
+		System.out.println(DateUtils.formatDate(date));
 	}
 
 }
