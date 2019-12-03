7ce4398e19c250e3c0bbc5182ed5e87f16905477
simple4h <crescentcxm@gmail.com>
2019-12-03 15:33:06 +0800

    update test

--- simple4h-product/src/test/java/com/simple4h/Simple4hProductApplicationTests.java
+++ simple4h-product/src/test/java/com/simple4h/Simple4hProductApplicationTests.java
@@ -1,18 +1,13 @@
 package com.simple4h;
 
-import com.simple4h.utils.DateUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
 
 @SpringBootTest
 class Simple4hProductApplicationTests {
 
-	@Test
-	void contextLoads() {
-		Date date = new Date();
-		System.out.println(DateUtils.formatDate(date));
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
